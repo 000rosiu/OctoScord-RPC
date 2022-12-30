8cacddb6e4fe6e14e80e2f22b2707f015d0e1091
000rosiu <73873042+000rosiu@users.noreply.github.com>
2022-12-30 21:03:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import 
+import win32gui
+import win32con
+import discord_rpc
+
+hwnd = win32gui.GetForegroundWindow()
+window_title = win32gui.GetWindowText(hwnd)
+
+current_window = window_title
+
+prev_hwnd = win32gui.GetWindow(hwnd, win32con.GW_HWNDPREV)
+prev_window = win32gui.GetWindowText(prev_hwnd)
+
+rpc = discord_rpc.DiscordRPC()
+rpc.set_presence(
+    details=f"Currently using: {current_window}",
+    state=f"Previously used: {prev_window}"
+)
+
+rpc.send_presence()
